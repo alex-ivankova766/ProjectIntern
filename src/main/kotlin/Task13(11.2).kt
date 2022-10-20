cb9f31ae2469e2cdc68aeacb6683ce2bcab906ab
immersion <radadabro@gmail.com>
2022-10-20 10:59:55 +0500

    Task13 с исправлениями

--- src/main/kotlin/Task13(11.2).kt
+++ src/main/kotlin/Task13(11.2).kt
@@ -3,6 +3,6 @@
 // а тело преобразовывает их в одну строку, в порядке возрастания(по количеству символов в слове) и выводит на экран.
 fun main() {
     val concat: (String, String, String) -> Unit =
-        { str1, str2, str3 -> println(listOf(str1, str2, str3).sortedWith(compareBy { it.length }).joinToString()) }
+        { str1, str2, str3 -> println(listOf(str1, str2, str3).sortedBy { it.length }.joinToString()) }
     concat("books", "crocodile", "dino")
 }
